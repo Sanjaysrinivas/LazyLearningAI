12f1a79fcdd0e029741e3480d36ce8800d8f9224
Sanjaysrinivas <sanjaysrinivas47@gmail.com>
2024-09-09 14:51:03 +0200

    imported necessary imports for importing the imports

---
+++ EmoClassifier3000/sentiment_analysis.py
@@ -0,0 +1,7 @@
+# MoodSwingAnalyzer: Because machines know your feelings better than you do.
+
+from textblob import TextBlob
+from transformers import pipeline
+
+
+# Function to analyze sentiment using TextBlob
